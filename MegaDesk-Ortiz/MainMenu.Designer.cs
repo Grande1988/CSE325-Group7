c9eb120280bdc669522b284a90d31cf5a43eee8e
Grande1988 <legrandredd@byui.edu>
2023-05-18 18:31:09 -0700

    made changes, Added Add Quote Button

--- MegaDesk-Ortiz/MainMenu.Designer.cs
+++ MegaDesk-Ortiz/MainMenu.Designer.cs
@@ -1,114 +1,135 @@
 ﻿namespace MegaDesk_Ortiz
 {
-    partial class MainMenu
-    {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+   partial class MainMenu
+   {
+      /// <summary>
+      ///  Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+      /// <summary>
+      ///  Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
 
-        #region Windows Form Designer generated code
+      #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            AddQuote = new Button();
-            SearchQuotes = new Button();
-            ViewAllQuotes = new Button();
-            Exit = new Button();
-            label1 = new Label();
-            SuspendLayout();
-            // 
-            // AddQuote
-            // 
-            AddQuote.Location = new Point(22, 103);
-            AddQuote.Name = "AddQuote";
-            AddQuote.Size = new Size(112, 23);
-            AddQuote.TabIndex = 0;
-            AddQuote.Text = "AddQuote";
-            AddQuote.UseVisualStyleBackColor = true;
-            AddQuote.Click += AddQuote_Click;
-            // 
-            // SearchQuotes
-            // 
-            SearchQuotes.Location = new Point(22, 147);
-            SearchQuotes.Name = "SearchQuotes";
-            SearchQuotes.Size = new Size(112, 23);
-            SearchQuotes.TabIndex = 1;
-            SearchQuotes.Text = "SearchQuotes";
-            SearchQuotes.UseVisualStyleBackColor = true;
-            SearchQuotes.Click += SearchQuotes_Click;
-            // 
-            // ViewAllQuotes
-            // 
-            ViewAllQuotes.Location = new Point(22, 192);
-            ViewAllQuotes.Name = "ViewAllQuotes";
-            ViewAllQuotes.Size = new Size(112, 23);
-            ViewAllQuotes.TabIndex = 2;
-            ViewAllQuotes.Text = "ViewAllQuotes";
-            ViewAllQuotes.UseVisualStyleBackColor = true;
-            ViewAllQuotes.Click += ViewAllQuotes_Click;
-            // 
-            // Exit
-            // 
-            Exit.Location = new Point(22, 235);
-            Exit.Name = "Exit";
-            Exit.Size = new Size(112, 23);
-            Exit.TabIndex = 3;
-            Exit.Text = "Exit";
-            Exit.UseVisualStyleBackColor = true;
-            Exit.Click += Exit_Click;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(171, 29);
-            label1.Name = "label1";
-            label1.Size = new Size(125, 25);
-            label1.TabIndex = 4;
-            label1.Text = "MegaDesk 2.0";
-            label1.Click += label1_Click;
-            // 
-            // MainMenu
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(484, 311);
-            Controls.Add(label1);
-            Controls.Add(Exit);
-            Controls.Add(ViewAllQuotes);
-            Controls.Add(SearchQuotes);
-            Controls.Add(AddQuote);
-            Name = "MainMenu";
-            StartPosition = FormStartPosition.CenterScreen;
-            Text = "Menu";
-            ResumeLayout(false);
-            PerformLayout();
-        }
+      /// <summary>
+      ///  Required method for Designer support - do not modify
+      ///  the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenu));
+         AddQuote = new Button();
+         SearchQuotes = new Button();
+         ViewAllQuotes = new Button();
+         Exit = new Button();
+         label1 = new Label();
+         pictureBox1 = new PictureBox();
+         ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+         SuspendLayout();
+         // 
+         // AddQuote
+         // 
+         AddQuote.Location = new Point(25, 137);
+         AddQuote.Margin = new Padding(3, 4, 3, 4);
+         AddQuote.Name = "AddQuote";
+         AddQuote.Size = new Size(128, 31);
+         AddQuote.TabIndex = 0;
+         AddQuote.Text = "AddQuote";
+         AddQuote.UseVisualStyleBackColor = true;
+         AddQuote.Click += AddQuote_Click;
+         // 
+         // SearchQuotes
+         // 
+         SearchQuotes.Location = new Point(25, 196);
+         SearchQuotes.Margin = new Padding(3, 4, 3, 4);
+         SearchQuotes.Name = "SearchQuotes";
+         SearchQuotes.Size = new Size(128, 31);
+         SearchQuotes.TabIndex = 1;
+         SearchQuotes.Text = "SearchQuotes";
+         SearchQuotes.UseVisualStyleBackColor = true;
+         SearchQuotes.Click += SearchQuotes_Click;
+         // 
+         // ViewAllQuotes
+         // 
+         ViewAllQuotes.Location = new Point(25, 256);
+         ViewAllQuotes.Margin = new Padding(3, 4, 3, 4);
+         ViewAllQuotes.Name = "ViewAllQuotes";
+         ViewAllQuotes.Size = new Size(128, 31);
+         ViewAllQuotes.TabIndex = 2;
+         ViewAllQuotes.Text = "ViewAllQuotes";
+         ViewAllQuotes.UseVisualStyleBackColor = true;
+         ViewAllQuotes.Click += ViewAllQuotes_Click;
+         // 
+         // Exit
+         // 
+         Exit.Location = new Point(25, 313);
+         Exit.Margin = new Padding(3, 4, 3, 4);
+         Exit.Name = "Exit";
+         Exit.Size = new Size(128, 31);
+         Exit.TabIndex = 3;
+         Exit.Text = "Exit";
+         Exit.UseVisualStyleBackColor = true;
+         Exit.Click += Exit_Click;
+         // 
+         // label1
+         // 
+         label1.AutoSize = true;
+         label1.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+         label1.Location = new Point(195, 39);
+         label1.Name = "label1";
+         label1.Size = new Size(151, 30);
+         label1.TabIndex = 4;
+         label1.Text = "MegaDesk 2.0";
+         label1.Click += label1_Click;
+         // 
+         // pictureBox1
+         // 
+         pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+         pictureBox1.Location = new Point(298, 168);
+         pictureBox1.Name = "pictureBox1";
+         pictureBox1.Size = new Size(164, 119);
+         pictureBox1.TabIndex = 5;
+         pictureBox1.TabStop = false;
+         pictureBox1.Click += pictureBox1_Click;
+         // 
+         // MainMenu
+         // 
+         AutoScaleDimensions = new SizeF(8F, 20F);
+         AutoScaleMode = AutoScaleMode.Font;
+         ClientSize = new Size(553, 415);
+         Controls.Add(pictureBox1);
+         Controls.Add(label1);
+         Controls.Add(Exit);
+         Controls.Add(ViewAllQuotes);
+         Controls.Add(SearchQuotes);
+         Controls.Add(AddQuote);
+         Margin = new Padding(3, 4, 3, 4);
+         Name = "MainMenu";
+         StartPosition = FormStartPosition.CenterScreen;
+         Text = "Menu";
+         ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+         ResumeLayout(false);
+         PerformLayout();
+      }
 
-        #endregion
+      #endregion
 
-        private Button AddQuote;
-        private Button SearchQuotes;
-        private Button ViewAllQuotes;
-        private Button Exit;
-        private Label label1;
-    }
+      private Button AddQuote;
+      private Button SearchQuotes;
+      private Button ViewAllQuotes;
+      private Button Exit;
+      private Label label1;
+      private PictureBox pictureBox1;
+   }
 }
--- MegaDesk-Ortiz/MainMenu.cs
+++ MegaDesk-Ortiz/MainMenu.cs
@@ -1,45 +1,50 @@
 namespace MegaDesk_Ortiz
 {
-    public partial class MainMenu : Form
-    {
-        public MainMenu()
-        {
-            InitializeComponent();
-        }
+   public partial class MainMenu : Form
+   {
+      public MainMenu()
+      {
+         InitializeComponent();
+      }
 
-        private void AddQuote_Click(object sender, EventArgs e)
-        {
-            AddQuoteForm viewAddQuoteForm = new AddQuoteForm();
-            viewAddQuoteForm.Tag = this;
-            viewAddQuoteForm.Show(this);
-            Hide();
-        }
+      private void AddQuote_Click(object sender, EventArgs e)
+      {
+         AddQuoteForm viewAddQuoteForm = new AddQuoteForm();
+         viewAddQuoteForm.Tag = this;
+         viewAddQuoteForm.Show(this);
+         Hide();
+      }
 
-        private void SearchQuotes_Click(object sender, EventArgs e)
-        {
-            SearchQuotesForm viewSearchQuotesFrom = new SearchQuotesForm();
-            viewSearchQuotesFrom.Tag = this;
-            viewSearchQuotesFrom.Show(this);
-            Hide();
-        }
+      private void SearchQuotes_Click(object sender, EventArgs e)
+      {
+         SearchQuotesForm viewSearchQuotesFrom = new SearchQuotesForm();
+         viewSearchQuotesFrom.Tag = this;
+         viewSearchQuotesFrom.Show(this);
+         Hide();
+      }
 
-        private void ViewAllQuotes_Click(object sender, EventArgs e)
-        {
-            ViewAllQuotesForm viewViewAllQuotes = new ViewAllQuotesForm();
-            viewViewAllQuotes.Tag = this;
-            viewViewAllQuotes.Show(this);
-            Hide();
+      private void ViewAllQuotes_Click(object sender, EventArgs e)
+      {
+         ViewAllQuotesForm viewViewAllQuotes = new ViewAllQuotesForm();
+         viewViewAllQuotes.Tag = this;
+         viewViewAllQuotes.Show(this);
+         Hide();
 
-        }
+      }
 
-        private void Exit_Click(object sender, EventArgs e)
-        {
-            Close();
-        }
+      private void Exit_Click(object sender, EventArgs e)
+      {
+         Close();
+      }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
+      private void label1_Click(object sender, EventArgs e)
+      {
 
-        }
-    }
+      }
+
+      private void pictureBox1_Click(object sender, EventArgs e)
+      {
+
+      }
+   }
 }
